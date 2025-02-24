8d509e3383b6fa41bec9fe946bdb252f63cdcfae
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-02-24 20:34:13 -0300

    feat: Resgatando dados do formulário com react-hook-form

--- src/pages/app/home.tsx
+++ src/pages/app/home.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
@@ -8,23 +9,52 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+
+const formSchema = z.object({
+  nome: z.string(),
+  login: z.string(),
+  ramal: z.string(),
+  patrimonio: z.string(),
+  informacao: z.string(),
+  local: z.string(),
+});
+
+type formType = z.infer<typeof formSchema>;
+
 export function Home() {
+  const { register, handleSubmit, control, watch } = useForm<formType>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const localWaatched = watch("local");
+
+  function handleSubmitFormTicket(data: formType) {
+    console.log(data);
+  }
+
   return (
-    <form className="dark:bg-accent border-accent-foreground/10 flex w-1/2 flex-col items-center justify-center gap-5 rounded-md border p-5">
+    <form
+      onSubmit={handleSubmit(handleSubmitFormTicket)}
+      className="dark:bg-accent border-accent-foreground/10 flex w-1/2 flex-col items-center justify-center gap-5 rounded-md border p-5"
+    >
       <div className="flex w-full gap-3">
         <div className="flex w-full flex-col gap-3">
           <Label>Nome</Label>
           <Input
+            {...register("nome")}
             type="text"
-            name="name"
             className="border-accent-foreground/15 bg-zinc-100 dark:bg-zinc-950"
           />
         </div>
         <div className="flex w-full flex-col gap-3">
           <Label>Login</Label>
+
           <Input
+            {...register("login")}
             type="text"
-            name="name"
             className="border-accent-foreground/15 bg-zinc-100 dark:bg-zinc-950"
           />
         </div>
@@ -33,16 +63,16 @@ export function Home() {
         <div className="flex w-full flex-col gap-3">
           <Label>Ramal</Label>
           <Input
+            {...register("ramal")}
             type="text"
-            name="name"
             className="border-accent-foreground/15 bg-zinc-100 dark:bg-zinc-950"
           />
         </div>
         <div className="flex w-full flex-col gap-3">
           <Label>Patrimônio</Label>
           <Input
+            {...register("patrimonio")}
             type="text"
-            name="name"
             className="border-accent-foreground/15 bg-zinc-100 dark:bg-zinc-950"
           />
         </div>
@@ -51,8 +81,8 @@ export function Home() {
       <div className="w-full">
         <Label>Informação</Label>
         <Input
+          {...register("informacao")}
           type="text"
-          name="name"
           className="border-accent-foreground/15 bg-zinc-100 dark:bg-zinc-950"
         />
       </div>
@@ -61,25 +91,44 @@ export function Home() {
         <div className="flex w-full flex-col gap-3">
           <Label>Local</Label>
           <Input
+            {...register("local")}
+            value={localWaatched}
             type="text"
-            name="name"
             className="border-accent-foreground/15 bg-zinc-100 dark:bg-zinc-950"
           />
         </div>
         <div className="flex w-full flex-col gap-3">
           <Label>Gabinetes</Label>
-          <Select>
-            <SelectTrigger className="border-accent-foreground/15 bg-zinc-100 dark:bg-zinc-950">
-              <SelectValue placeholder="Theme" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value=" "></SelectItem>
-              <SelectItem value="light">Opção 1</SelectItem>
-              <SelectItem value="dark">Opção 2</SelectItem>
-              <SelectItem value="system">Opção 3</SelectItem>
-            </SelectContent>
-          </Select>
+
+          <Controller
+            control={control}
+            name="local"
+            render={({ field: { name, onChange, value, disabled } }) => {
+              return (
+                <Select
+                  defaultValue=" "
+                  name={name}
+                  onValueChange={onChange}
+                  value={value}
+                  disabled={disabled}
+                >
+                  <SelectTrigger className="border-accent-foreground/15 bg-zinc-100 dark:bg-zinc-950">
+                    <SelectValue placeholder="Selecione..." />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value=" "></SelectItem>
+                    <SelectItem value="Local 1">Local 1</SelectItem>
+                    <SelectItem value="Local 2">Local 2</SelectItem>
+                    <SelectItem value="Local 3">Local 3</SelectItem>
+                  </SelectContent>
+                </Select>
+              );
+            }}
+          ></Controller>
         </div>
+      </div>
+      <div className="flex w-full justify-start">
+        <Button type="submit">Registrar</Button>
       </div>
     </form>
   );
